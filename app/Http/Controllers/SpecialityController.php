fafb457444f68f8db21eb8f863b647a1f3c75695
Josephglz <joseph220199@gmail.com>
2024-11-09 21:02:52 -0600

    feat: Implement speciality CRUD and doctor count
    Added API endpoints for managing specialities (create, read, update, delete) and retrieving the number of doctors associated with a speciality.  This allows for better management of speciality data and provides a convenient way to access related doctor counts.

--- app/Http/Controllers/SpecialityController.php
+++ app/Http/Controllers/SpecialityController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Speciality;
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class SpecialityController extends Controller
 {
-    //
+    public function index()
+    {
+        $specialities = Speciality::all();
+        foreach ($specialities as $speciality) {
+            $doctors = Doctor::where('speciality_id', $speciality->id)->get();
+            $speciality->doctors = count($doctors);
+        }
+        return response()->json($specialities);
+    }
+
+    public function show($id)
+    {
+        $speciality = Speciality::find($id);
+        return response()->json($speciality);
+    }
+
+    public function store(Request $request)
+    {
+        $speciality = new Speciality();
+        $speciality->name = $request->name;
+        $speciality->save();
+        return response()->json($speciality);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $speciality = Speciality::find($id);
+        $speciality->name = $request->name;
+        $speciality->save();
+        return response()->json($speciality);
+    }
+
+    public function destroy($id)
+    {
+        $speciality = Speciality::find($id);
+        $speciality->delete();
+        return response()->json($speciality);
+    }
+
+    public function getCountDoctors($id) {
+        $speciality = Speciality::find($id);
+        $doctors = Doctor::where('speciality_id', $speciality->id)->get();
+        return response()->json(count($doctors));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Dashboard;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SpecialityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ Route::resource('doctors', DoctorController::class);
 Route::resource('users', UserController::class);
 Route::resource('appointments', AppointmentController::class);
 Route::resource('patients', PatientController::class);
+Route::resource('specialties', SpecialityController::class);
 
 Route::post('users/login', [UserController::class, 'login']);
 Route::get('dashboard', [Dashboard::class, 'index']);
