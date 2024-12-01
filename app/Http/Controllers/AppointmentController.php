2de13df97edf15ded2d2f61030496573350246f6
Josephglz <joseph220199@gmail.com>
2024-12-01 00:13:54 -0600

    fix update status appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -55,7 +55,7 @@ class AppointmentController extends Controller
     public function updateStatus(Request $request, $id) {
         $appointment = Appointment::find($id);
         $appointment->status = $request->status;
-        $appointment->save();
+        $appointment->update($request->all());
         return response()->json($appointment);
     }
 }
